1f0505e8b1c640f5cfe2c2b589506a56fe8c1d25
Dylan Hercher <dhercher@colgate.edu>
2013-03-29 16:46:34 -0400

    added user and expereince model files

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  attr_accessible :title, :rating, :description, :release_date, :director
+  def self.all_ratings
+    %w(G PG PG-13 NC-17 R)
+  end
+  def self.same_director director
+  	User.find_by_director(director)
+  end
+end
