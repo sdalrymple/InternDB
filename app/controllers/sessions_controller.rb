4bce850ef588928171f415efccff571b50d37867
Sean Dalrymple <sdalrymple@colgate.edu>
2013-05-05 22:10:04 -0400

    redirect from logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,6 +30,6 @@ class SessionsController < ApplicationController
 
   def destroy
   	session[:user_id] = nil
-  	render :text => "You've logged out!"
+  	redirect_to '/login' and return
   end
 end
